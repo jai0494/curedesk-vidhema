13ddb8e21f5fd9d5d9ef29aa06f3e1e37e372ad9
Jaikant Chaturvedi <jaikant94chaturvedi@gmail.com>
2024-09-27 11:48:19 +0530

    Design

--- src/component/TextImageSection.tsx
+++ src/component/TextImageSection.tsx
@@ -1,6 +1,6 @@
 const TextImageSection = () => {
   return (
-    <section className="bg-[#fffaf9] flex flex-col items-center justify-center p-8">
+    <section className="bg-[#fffaf9] flex flex-col items-center justify-center pt-8 pb-0 px-8">
       <h2 className="font-display text-3xl tracking-tight text-slate-900 font-nunito font-bold sm:text-4xl md:max-w-[1000px] text-center">
       Unlock The Full Spectrum Of Healthcare Management With Our All-In-One Solution
 
@@ -9,9 +9,9 @@ const TextImageSection = () => {
             Because you’d probably be a little confused if we suggested you complicate your everyday business tasks instead.
           </p>
       <img
-        src="/All-in-one-solution-of-Healthray.webp" // Replace with your image URL
+        src="/All-in-one-solution-of-Curedesk.jpg" // Replace with your image URL
         alt="Descriptive Alt Text"
-        className="w-full max-w-xl rounded-lg"
+        className="w-full max-w-[800px] rounded-lg"
       />
     </section>
   );
