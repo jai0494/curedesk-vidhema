3690c632b3ab136adf8c848959543b7647b51f77
Jaikant Chaturvedi <jaikant94chaturvedi@gmail.com>
2024-09-28 18:02:46 +0530

    Link

--- src/component/TrustedByCompanies.tsx
+++ src/component/TrustedByCompanies.tsx
@@ -7,7 +7,7 @@ const TrustedByCompanies = () => {
       <div className="max-w-7xl mx-auto relative">
         <div className="text-center max-w-xl mx-auto">
           <h3 className="text-3xl md:text-4xl/tight font-bold text-black mt-4 font-nunito">
-            Trusted by Leading Companies
+            Trusted by Leading Hospital
           </h3>
         </div>
 
