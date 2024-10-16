a7a224c206a3834901e09a9978e048a26f753e16
Jaikant Chaturvedi <jaikant94chaturvedi@gmail.com>
2024-10-16 19:21:29 +0530

    Home Change Refral Feild

--- src/component/ServiceMain.tsx
+++ src/component/ServiceMain.tsx
@@ -11,7 +11,7 @@ const ServiceMain: React.FC<ServiceMainProps> = ({ heading, subheading }) => {
       className="relative pt-5 md:pt-20 md:pb-32 md:pb-20 overflow-x-hidden from-slate-500/10 bg-[url(https://firebasestorage.googleapis.com/v0/b/curedesk-3593d.appspot.com/o/bg-1.png?alt=media&token=6a70ce93-53a1-4abd-b473-1498837a4ba9)] bg-no-repeat bg-cover active"
       id="home"
     >
-      <div className="mx-auto max-w-7xl">
+      <div className="mx-auto max-w-7xl px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 items-center md:px-4 lg:px-0">
           <div className="text-sm md:py-20 px-4 lg:px-10 md:py-0">
             <h1 className="md:text-6xl/tight text-4xl text-dark tracking-normal leading-normal text-slate-900 font-bold font-nunito mb-4 mt-6">
