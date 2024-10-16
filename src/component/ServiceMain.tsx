d447162aedf19bd5696722fe62cb6d3c3f0024fb
Jaikant Chaturvedi <jaikant94chaturvedi@gmail.com>
2024-10-16 19:06:50 +0530

    Home Change Refral Feild

--- src/component/ServiceMain.tsx
+++ src/component/ServiceMain.tsx
@@ -8,7 +8,7 @@ interface ServiceMainProps {
 const ServiceMain: React.FC<ServiceMainProps> = ({ heading, subheading }) => {
   return (
     <section
-      className="relative pt-5 md:pt-32 md:pt-20 md:pb-32 md:pb-20 overflow-x-hidden from-slate-500/10 bg-[url(https://firebasestorage.googleapis.com/v0/b/curedesk-3593d.appspot.com/o/bg-1.png?alt=media&token=6a70ce93-53a1-4abd-b473-1498837a4ba9)] bg-no-repeat bg-cover active"
+      className="relative pt-5 md:pt-20 md:pb-32 md:pb-20 overflow-x-hidden from-slate-500/10 bg-[url(https://firebasestorage.googleapis.com/v0/b/curedesk-3593d.appspot.com/o/bg-1.png?alt=media&token=6a70ce93-53a1-4abd-b473-1498837a4ba9)] bg-no-repeat bg-cover active"
       id="home"
     >
       <div className="mx-auto max-w-7xl">
